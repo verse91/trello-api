dcd7590772ea09789ff0947a67c42d10ea187298
VERSE <sunnyone0901@gmail.com>
2025-01-19 11:58:05 +0700

    handle response error stack

--- src/config/environment.ts
+++ src/config/environment.ts
@@ -1,1 +1,13 @@
+import 'dotenv/config';
 
+export const env = {
+    MONGODB_URI: process.env.MONGODB_URI,
+    DB_NAME:process.env.DB_NAME,
+
+    HOST:process.env.HOST,
+    PORT:process.env.PORT,
+
+    BUILD_MODE:process.env.BUILD_MODE,
+
+    AUTHOR:process.env.AUTHOR,
+}
